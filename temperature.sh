3b9c3f073e1bc548667e0171a5e036cb759ee953
David Nuss <dmnuss@ksu.edu>
2018-09-10 14:48:29 -0500

    Added Kelvin conversion

--- temperature.sh
+++ temperature.sh
@@ -4,9 +4,11 @@
 #Example: ./temperature.sh 5
 
 c=$( echo "scale=2; ($1-32)*5/9" | bc -l )
+k=$( echo "scale=2; ($c+273.15)" | bc -l )
 
 echo "The temperature in Fahrenheit: $1"
 echo "The temperature in Celsius: $c" 
+echo "The temperature in Kelvin: $k"
 
 
 
